1ae0084006ece9a3d509b32d4c1f137fd89ae5c5
Chysev <Johnlayda92@gmail.com>
2023-02-08 08:14:02 -0500

    connection erela.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,11 +1,18 @@
 const conf = require("../config.json");
 const { Events } = require("discord.js");
+const Manager = require("../connections/manager.js");
 const client = require("../handlers/client.js");
 
 module.exports = {
   name: Events.MessageCreate,
   async execute(message) {
     if (!message.content.startsWith(conf.PREFIX) || message.author.bot) return;
+
+    const connection = Manager.create({
+      guild: message.guild.id,
+      voiceChannel: message.member.voice.channel.id,
+      textChannel: message.channel.id,
+    });
 
     const args = message.content.slice(conf.PREFIX.length).trim().split(/ +/);
     const command = args.shift().toLowerCase();
@@ -24,7 +31,7 @@ module.exports = {
         client.commands.find(
           (commands) => commands.aliases && commands.aliases.includes(command)
         );
-      execute_commands.execute(client, message, args);
+      execute_commands.execute(client, message, args, connection);
     } catch (error) {
       console.error(error);
       await message.reply("There was an error trying to execute that command!");
