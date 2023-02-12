a20e04f5138799f98f096f22838859fe26949a6e
Chysev <Johnlayda92@gmail.com>
2023-02-12 14:31:08 +0400

    removed connections because of not id not found in other commands

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,18 +1,11 @@
 const conf = require("../config.json");
 const { Events } = require("discord.js");
-const Manager = require("../connections/manager.js");
 const client = require("../handlers/client.js");
 
 module.exports = {
   name: Events.MessageCreate,
   async execute(message) {
     if (!message.content.startsWith(conf.PREFIX) || message.author.bot) return;
-
-    const connection = Manager.create({
-      guild: message.guild.id,
-      voiceChannel: message.member.voice.channel.id,
-      textChannel: message.channel.id,
-    });
 
     const args = message.content.slice(conf.PREFIX.length).trim().split(/ +/);
     const command = args.shift().toLowerCase();
@@ -31,7 +24,7 @@ module.exports = {
         client.commands.find(
           (commands) => commands.aliases && commands.aliases.includes(command)
         );
-      execute_commands.execute(client, message, args, connection, Manager);
+      execute_commands.execute(client, message, args);
     } catch (error) {
       console.error(error);
       await message.reply("There was an error trying to execute that command!");
